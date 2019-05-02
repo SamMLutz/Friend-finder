df068db9c4f5e9232ae47c70f0dd6dd24d8bebe1
samlutz <sammlutz@gmail.com>
2019-05-02 11:47:07 -0400

    classes and ids added

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsList = [
+    {
+        "name": "Sam",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjLqPzZkf3hAhXsct8KHQINDTYQjRx6BAgBEAU&url=http%3A%2F%2Fwww.fanpop.com%2Fclubs%2Fleonardo-dicaprio%2Fimages%2F25975086%2Ftitle%2Frandom-photoshoot-photo&psig=AOvVaw0raUprdr7aIwAGvf0zVFnS&ust=1556896530528360",
+        "scores": [
+            5,
+            1,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+
+module.exports = friendsList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,19 @@
 var path = require("path")
 
+var friendsData = require("../data/friends")
+
 // Routes
 // =============================================================
 module.exports = function(app) {
 
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+      });
 
-
+    app.post("/api/friends", function (req, res) {
+        friendsData.push(req.body);
+        // res.json(true);
+    })
+    // console.log(req.body)
 };
+// console.log(friendsData)
