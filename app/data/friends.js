e2f6ec56a4234ff9196ecfd14807d8302a8f80bf
samlutz <sammlutz@gmail.com>
2019-05-03 21:21:43 -0400

    first friends loop complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friendsList = [
     {
-        "name": "Sam",
-        "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjLqPzZkf3hAhXsct8KHQINDTYQjRx6BAgBEAU&url=http%3A%2F%2Fwww.fanpop.com%2Fclubs%2Fleonardo-dicaprio%2Fimages%2F25975086%2Ftitle%2Frandom-photoshoot-photo&psig=AOvVaw0raUprdr7aIwAGvf0zVFnS&ust=1556896530528360",
-        "scores": [
+        name: "Sam",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjLqPzZkf3hAhXsct8KHQINDTYQjRx6BAgBEAU&url=http%3A%2F%2Fwww.fanpop.com%2Fclubs%2Fleonardo-dicaprio%2Fimages%2F25975086%2Ftitle%2Frandom-photoshoot-photo&psig=AOvVaw0raUprdr7aIwAGvf0zVFnS&ust=1556896530528360",
+        scores: [
             5,
             1,
             4,
@@ -17,9 +17,14 @@ var friendsList = [
 ]
 
 module.exports = friendsList;
+var total = 0;
 
-// for (var i =0; friendsList.scores.length; i++) {
-//     friendsList.scores[i]
-// }
+for (var i = 0; i < friendsList[0].scores.length; i++) {
+    
+    total = total + parseInt(friendsList[0].scores[i])
+    // console.log(friendsList[0].scores[i])
+    // console.log(total);
+}
+console.log(total);
+// console.log(friendsList[0].scores)
 
-console.log(friendsList.name)
